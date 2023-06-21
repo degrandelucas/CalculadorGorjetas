1a8eec39e46fb5e3b33d6202f475ddd8aefcb230
degrandelucas <lucasdegrande15@gmail.com>
2023-06-21 00:49:30 -0300

    included firts file .js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function calculateTips(event) {}
+document.getElementById("TipsTotal").style.display = "none";
+document.getElementById("eachpeople").style.display = "none";
+document.getElementById("TipsForm").addEventListener("submit", calculateTips);
